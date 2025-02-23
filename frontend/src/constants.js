df7d50c3d7e56a29ed9899c1158ba51ec3f470f3
Sourav Budke <104896995+Souravbudke@users.noreply.github.com>
2025-02-23 23:14:07 +0530

    Revert "Update constants.js"
    This reverts commit 2cd32385899555e960eef6cae423eafdb29979db.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
+export const BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://westernstreet-api.vercel.app'  // Replace with your actual backend Vercel URL
+  : 'http://localhost:5001';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
