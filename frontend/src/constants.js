2cd32385899555e960eef6cae423eafdb29979db
Sourav Budke <104896995+Souravbudke@users.noreply.github.com>
2025-02-23 23:05:57 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://westernstreet-api.vercel.app'  // Replace with your actual backend Vercel URL
-  : 'http://localhost:5001';
+export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
